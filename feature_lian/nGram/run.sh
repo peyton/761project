a2903a3ce8535556d5c1fe81669091e5acfe1860
Chengliang Lian <clian@unix6.andrew.cmu.edu>
2015-04-13 20:24:41 -0400

    add 4Gram Feature and Token-type Feature, LDA running is too slow

--- feature_lian/nGram/run.sh
+++ feature_lian/nGram/run.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+#this bash should run on andrew machine since all the bin file it depends runs under linux file system
 trainFile=LM-100MW-Tool
-trainFilePath=../intermediateData/${trainFile}.txt
-testfile=developmentSet.dat
-testfilePath=../data/${testfile}
-path=../CMU-Cam-Tool/
+trainFilePath=./data/${trainFile}.txt
+testfile=${2}  #developmentSet.dat_1
+testfilePath=./data/${testfile}
+path=../bin/
 kGram=${1} #use k gram
+method=-witten_bell
 #build vocab, part1 and part2
 #cat ${trainFilePath} | ${path}/text2wfreq | ${path}/wfreq2vocab -top 10000 > ${trainFile}.vocab
 #Generate a k-gram of the training text, based on this vocabulary, convert it into language model
 #cat ${trainFilePath} | ${path}/text2idngram -n ${kGram} -vocab ${trainFile}.vocab > ${trainFile}.idngram
-#${path}/idngram2lm -vocab ${trainFile}.vocab -idngram ${trainFile}.idngram -context context.ccs -binary ${trainFile}.lm -arpa ${trainFile}.arpa
-cat ${trainFilePath} | ${path}/text2idngram -n ${kGram} -vocab ${trainFile}.vocab | ${path}/idngram2lm -vocab ${trainFile}.vocab -idngram - -context context.ccs -binary ${trainFile}.lm -arpa ${trainFile}.arpa
+#${path}/idngram2lm ${method} -idngram ${trainFile}.idngram -context context.ccs -n ${kGram} -four_byte_counts -vocab ${trainFile}.vocab -arpa ${trainFile}.arpa -cutoffs 4 4 4
 #test on reviews-test data-set
-#echo "${path}/perplexity -probs ${testfile}.probs -text ${testfilePath}" | ${path}/evallm -arpa ${trainFile}.arpa -context context.ccs
+while read line
+do
+    #echo "perplexity -text ${line}" | ${path}/evallm -arpa ${trainFile}.arpa -context context.ccs >> ${testfilePath}_out_${kGram}gram
+    echo "${line}" > tmp
+    echo "perplexity -text tmp" | ${path}/evallm -arpa ${trainFile}.arpa -context context.ccs | grep "Entropy" >> ${testfilePath}_${1}gram_out
+done < ${testfilePath}
+
