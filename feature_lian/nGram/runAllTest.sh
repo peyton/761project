f2afabe56c44acfce7f2ba2716e35f2f060e2178
lianguasth <lianguasth@sina.com>
2015-04-26 21:33:11 +0000

    syn with andrew machine

---
+++ feature_lian/nGram/runAllTest.sh
@@ -0,0 +1,1 @@
+./runTest.sh 7 Bal
--- feature_lian/nGram/runTest.sh
+++ feature_lian/nGram/runTest.sh
@@ -2,9 +2,10 @@
 #this bash should run on andrew machine since all the bin file it depends runs under linux file system
 trainFile=LM-train-100MW
 trainFilePath=../../data/${trainFile}.txt
-testPath=/mnt/intermediateData/cmu
-fA=${testPath}test.dat
-fB=${testPath}train.dat
+Bal=${2} #if use balance data enter Bal
+testPath=/mnt/intermediateData/cmu${Bal}
+fA=${testPath}Test.dat
+fB=${testPath}Train.dat
 modelPath=/mnt/
 outPath=~/761project/features/cmufeat
 path=../CMU-Cam-Tool/bin/
@@ -24,7 +25,7 @@ genFeat(){
         echo "${line}" > tmp${kGram}
         str=$(sudo echo "perplexity -include_unks -text tmp${kGram}" | ${path}/evallm -binary ${modelPath}/${trainFile}_${kGram}.binlm | grep "Entropy")
         lenDoc=$(echo "${line}" | wc -w)
-        echo "${str},docLen = ${lenDoc}" >> ${outPath}/cmu${kGram}Gram${post}.txt
+        echo "${str},docLen = ${lenDoc}" >> ${outPath}/cmu${Bal}${kGram}Gram${post}.txt
     done < ${testFile}
 }
 
